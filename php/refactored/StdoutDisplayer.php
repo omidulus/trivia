a4e19e3e17aa44a9e328cdee5531d1086b48e5c5
Toma Marius Tomy <tomy.toma@emag.ro>
2022-02-19 23:11:22 +0200

    [My refactoring] Encapsulated echoln() in the new displayer

--- php/refactored/StdoutDisplayer.php
+++ php/refactored/StdoutDisplayer.php
@@ -7,14 +7,14 @@ class StdoutDisplayer
 
     public function displayPlayerAnsweredWrongly(string $playerName): void
     {
-        echoln("Question was incorrectly answered");
-        echoln($playerName . " was sent to the penalty box");
+        $this->echoln("Question was incorrectly answered");
+        $this->echoln($playerName . " was sent to the penalty box");
     }
 
     public function displayPlayerAnswersCorrectly(string $playerName, int $coinsAfterAnswer): void
     {
-        echoln("Answer was correct!!!!");
-        echoln(
+        $this->echoln("Answer was correct!!!!");
+        $this->echoln(
             $playerName
             . " now has "
             . $coinsAfterAnswer
@@ -24,44 +24,48 @@ class StdoutDisplayer
 
     public function displayPlayerAdded(string $playerName, int $playerNumber): void
     {
-        echoln($playerName . " was added");
-        echoln("They are player number " . $playerNumber);
+        $this->echoln($playerName . " was added");
+        $this->echoln("They are player number " . $playerNumber);
     }
 
     public function displayPlayerCategory(string $currentPlayerCategory): void
     {
-        echoln("The category is " . $currentPlayerCategory);
+        $this->echoln("The category is " . $currentPlayerCategory);
     }
 
     public function displayCurrentPlayerChanged(string $playerName): void
     {
-        echoln($playerName . " is the current player");
+        $this->echoln($playerName . " is the current player");
     }
 
     public function displayPlayerLeftThePenaltyBox(string $playerName): void
     {
-        echoln($playerName . " is getting out of the penalty box");
+        $this->echoln($playerName . " is getting out of the penalty box");
     }
 
     public function displayPlayerCouldNotLeavePenaltyBox(string $playerName): void
     {
-        echoln($playerName . " is not getting out of the penalty box");
+        $this->echoln($playerName . " is not getting out of the penalty box");
     }
 
     public function displayQuestion(string $question): void
     {
-        echoln($question);
+        $this->echoln($question);
     }
 
     public function displayRolledDie(int $roll): void
     {
-        echoln("They have rolled a " . $roll);
+        $this->echoln("They have rolled a " . $roll);
     }
 
     public function displayPlayerNewStatus(Player $currentPlayer): void
     {
-        echoln(
+        $this->echoln(
             sprintf("%s's new location is %d", $currentPlayer->name(), $currentPlayer->boardPlace())
         );
+    }
+
+    private function echoln(string $string) {
+        echo $string."\n";
     }
 }
